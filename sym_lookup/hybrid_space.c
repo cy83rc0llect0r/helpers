d1059a94246ab64ea28de7d8f7beef0d1a0689c5
AmirReza <121097799+cy83rc0llect0r@users.noreply.github.com>
2024-07-22 00:18:04 +0330

    Modify code to generate helper functions
    This is not a kernel module. This code can be used as a helper function for ftrace or something needs a symbols address. It can be used in a kernel module.

--- sym_lookup/hybrid_space.c
+++ sym_lookup/hybrid_space.c
@@ -1,34 +1,20 @@
-#include <linux/module.h>
-#include <linux/fs.h>
-#include <linux/uaccess.h>
-#include <linux/slab.h>
-#include <linux/kmod.h>
-
-#define INPUT_FILE "/tmp/sym_addr"
 #define PROGRAM_PATH "/path/to/userspace/file/reader/and/line/parser"
+#define INPUT_FILE "/path/to/userspace/file/reader/and/line/parser/output/file"
 
-static unsigned long sym_addr = 0;
-
-static int run_user_program(void) {
-    char * argv[] = {
-        PROGRAM_PATH,
-        NULL
-    };
-    char * envp[] = {
-        "HOME=/",
-        "PATH=/sbin:/bin:/usr/sbin:/usr/bin",
-        NULL
-    };
+static int run_user_program(void)
+{
+    char *argv[] = { PROGRAM_PATH, NULL };
+    char *envp[] = { "HOME=/", "PATH=/sbin:/bin:/usr/sbin:/usr/bin", NULL };
     return call_usermodehelper(argv[0], argv, envp, UMH_WAIT_PROC);
 }
 
-static int __init my_module_init(void) {
-    struct file * f;
-    char * buf;
+unsigned long sym_addr_lookup(void)
+{
+    struct file *f;
+    char *buf;
     loff_t pos = 0;
     int ret;
 
-    // Run the user-space program
     ret = run_user_program();
     if (ret) {
         pr_err("Failed to run user program: %d\n", ret);
@@ -38,7 +24,7 @@ static int __init my_module_init(void) {
     buf = kmalloc(32, GFP_KERNEL);
     if (!buf) {
         pr_err("Failed to allocate memory\n");
-        return -ENOMEM;
+        return -ENOMEM;;
     }
 
     f = filp_open(INPUT_FILE, O_RDONLY, 0);
@@ -57,21 +43,10 @@ static int __init my_module_init(void) {
         return ret;
     }
 
-    buf[ret] = '\0'; // Null-terminate the buffer
-    kstrtoul(buf, 16, & sym_addr);
-    pr_info("Address of %s: %lx\n", sym_addr);
+    buf[ret] = '\0';
+    kstrtoul(buf, 16, &sym_addr);
+    pr_info("Address of sym: %lx\n", sym_addr);
 
     kfree(buf);
-    return 0;
+    return sym_addr;
 }
-
-static void __exit my_module_exit(void) {
-    pr_info("Module exiting. Address of sym was: %lx\n", sym_addr);
-}
-
-module_init(my_module_init);
-module_exit(my_module_exit);
-
-MODULE_LICENSE("MIT");
-MODULE_AUTHOR("AmirReza(cy83rc0llect0r)");
-MODULE_DESCRIPTION("A module(helper) to find a sym address");
